59627765e170487f226204d9c8c1c6aba654e88f
Александр <109337223+by-Egorov@users.noreply.github.com>
2022-10-30 00:58:12 +0300

    translate content

--- js/lang.js
+++ js/lang.js
@@ -21,5 +21,22 @@ const langArr = {
 	"experience-subtitle": {
 		"ru": "Мои навыки",
 		"en": "My Abilities"
-	}
+	},
+	"experience-lang__title": {
+		"ru": "Языки программирования",
+		"en": "Programming languages"
+	},
+	"experience-lang__subtitle": {
+		"ru": "Более 6 месяцев",
+		"en": "More than 6 months"
+	},
+	"experience-tools__title": {
+		"ru": "Интсрументы",
+		"en": "Tools"
+	},
+	"experience-tools__subtitle": {
+		"ru": "Более 6 месяцев",
+		"en": "More than 6 months"
+	},
+
 }
